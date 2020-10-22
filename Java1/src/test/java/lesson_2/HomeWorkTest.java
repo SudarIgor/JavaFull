399848cdd78f6a4892c820aa7eda838e1ed9557f
IgorSudar <i.v.sudar@yandex.ru>
2020-10-22 20:04:57 +0400

    add test for HW2

---
+++ Java1/src/test/java/lesson_2/HomeWorkTest.java
@@ -0,0 +1,35 @@
+package lesson_2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HomeWorkTest {
+
+//    @Test
+//    public void test1() {
+//        HomeWork hw = new HomeWork();
+//        Assert.assertTrue(hw.checkArray(new int[]{1, 2, 3, 5, 1}));
+//        Assert.assertTrue(hw.checkArray(new int[]{1, 2, 1, 3, 1, 2, 5, 1}));
+//        Assert.assertTrue(hw.checkArray(new int[]{1, 2, 6, 3, 5, 1}));
+//        Assert.assertTrue(hw.checkArray(new int[]{1, -1}));
+//    }
+//
+//    @Test
+//    public void test2() {
+//        HomeWork hw = new HomeWork();
+//        Assert.assertFalse(hw.checkArray(new int[]{1, 2, 3, 5, 4}));
+//    }
+//
+//    @Test
+//    public void test3() {
+//        HomeWork hw = new HomeWork();
+//        Assert.assertArrayEquals(new int[]{2, 3, 4, 5, 1}, hw.shift(new int[]{1, 2, 3, 4, 5}, -1));
+//        Assert.assertArrayEquals(new int[]{3, 4, 5, 1, 2}, hw.shift(new int[]{1, 2, 3, 4, 5}, -2));
+//        Assert.assertArrayEquals(new int[]{6, 1, 2, 3, 4, 5}, hw.shift(new int[]{1, 2, 3, 4, 5, 6}, 1));
+//        Assert.assertArrayEquals(new int[]{5, 1, 2, 3, 4}, hw.shift(new int[]{1, 2, 3, 4, 5}, 1));
+//        Assert.assertArrayEquals(new int[]{1, 2, 3, 4, 5}, hw.shift(new int[]{1, 2, 3, 4, 5}, -5));
+//        Assert.assertArrayEquals(new int[]{2, 3, 4, 5, 1}, hw.shift(new int[]{1, 2, 3, 4, 5}, -6));
+//    }
+
+
+}

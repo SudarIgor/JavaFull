1b78d666517fb7d15affab25829da73ce764148b
IgorSudar <i.v.sudar@yandex.ru>
2020-12-17 00:50:11 +0400

    add HomeWork1 for Java3

---
+++ Java3/src/main/java/lesson1/Fruit.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class Fruit {
+    double weigh;
+    public Fruit() {
+        weigh=0;
+    }
+
+    public double getWeigh() {
+        return weigh;
+    }
+}
---
+++ Java3/src/main/java/lesson1/MyArray.java
@@ -0,0 +1,52 @@
+package lesson1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 1. Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
+ * 2. Написать метод, который преобразует массив в ArrayList;
+ * */
+
+public class MyArray <T> {
+    T [] object;
+    private  int size;
+
+
+    public MyArray(T...object) {
+        this.object = object;
+        size=object.length;
+    }
+
+    public Object[] moveItem(int ind1, int ind2){
+        try {
+            T oldInd1 =  object[ind1];
+            T oldInd2 =  object[ind2];
+            object [ind1] = oldInd2;
+            object [ind2] = oldInd1;
+        }catch (IndexOutOfBoundsException e){
+            if(ind1>=size) System.out.printf("index %d outside the array\n", ind1);
+            if(ind2>=size) System.out.printf("index %d outside the array\n", ind2);
+        }
+        return object;
+    }
+
+    public ArrayList<T> toArrayList(){
+        ArrayList<T> objectArrayList = new ArrayList <T> (Arrays.asList(this.object));
+        return objectArrayList;
+    }
+
+    public Object[] getObject() {
+        return object;
+    }
+
+    public void setObject(T[] object) {
+        this.object = object;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(object);
+    }
+}
